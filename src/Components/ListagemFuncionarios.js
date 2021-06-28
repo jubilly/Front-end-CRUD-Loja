415a5e5ae55851101eda7a44da9c3a62f8848466
Amanda Prates (Jubilly) <amandaprates-12@hotmail.com>
2021-06-28 19:56:14 -0300

    input busca

--- src/Components/ListagemFuncionarios.js
+++ src/Components/ListagemFuncionarios.js
@@ -15,7 +15,9 @@ export default class ListagemFuncionarios extends Component {
             <h2 className="wrapper__title">Lista de Funcionários</h2>
                 <div className="wrapper__filter">
                     <label className="wrapper__filter-label">Pesquisar...</label>
-                    <input className="wrapper__filter-input wrapper__filter-input--full" onChange={(event)=>{this.props.Filter(event)}} value={this.props.filterdata} placeholder="Digite aqui..."/>
+                    <input className="wrapper__filter-input wrapper__filter-input--full" 
+                    onChange={(event)=>{this.props.Filter(event)}} 
+                    value={this.props.filterdata} placeholder="Digite aqui..."/>
                 </div>
                     {this.props.mapArrayFilter}
                     {this.props.mapArray}
