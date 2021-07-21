1d27915c259823e5f17b339375497b5530d71fbd
Amanda Prates (Jubilly) <amandaprates-12@hotmail.com>
2021-07-21 19:42:52 -0300

    Listagem de Funcionarios

--- src/Components/ListagemFuncionarios.js
+++ src/Components/ListagemFuncionarios.js
@@ -8,20 +8,20 @@ export default class ListagemFuncionarios extends Component {
     }
 
     render(){
-        return (
-            <>
-            <div className="wrapper">
-                
-            <h2 className="wrapper__title">Lista de Funcionários</h2>
-                <div className="wrapper__filter">
-                    <label className="wrapper__filter-label">Pesquisar...</label>
-                    <input className="wrapper__filter-input wrapper__filter-input--full" 
-                    onChange={(event)=>{this.props.Filter(event)}} 
-                    value={this.props.filterdata} placeholder="Digite aqui..."/>
-                </div>
-                    {this.props.mapArrayFilter}
-                    {this.props.mapArray}
-            </div>
+return (
+    <>
+    <div className="wrapper">
+        
+    <h2 className="wrapper__title">Lista de Funcionários</h2>
+        <div className="wrapper__filter">
+            <label className="wrapper__filter-label">Pesquisar...</label>
+            <input className="wrapper__filter-input wrapper__filter-input--full" 
+            onChange={(event)=>{this.props.Filter(event)}} 
+            value={this.props.filterdata} placeholder="Digite aqui..."/>
+        </div>
+            {this.props.mapArrayFilter}
+            {this.props.mapArray}
+    </div>
             </>
         )
     }
